ce68be7e57255f36ff5a1342486685ac5f750362
Trevor Szabo <tszabo616@gmail.com>
2023-07-05 17:46:49 -0400

    Updated package versions to latest

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {AuthContextProvider} from './store/authContext';
+import { AuthContextProvider } from './store/authContext';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <React.StrictMode>
         <AuthContextProvider>
             <BrowserRouter>
                 <App />
             </BrowserRouter>
         </AuthContextProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
